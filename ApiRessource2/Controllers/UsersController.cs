80fb3d6745afa80daf543d9a92219d9a1529c583
Kevin-Moreau65 <kevin.moreau655@gmail.com>
2023-05-18 18:02:56 +0200

    Fix user creation

--- ApiRessource2/Controllers/UsersController.cs
+++ ApiRessource2/Controllers/UsersController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿//using ApiRessource2.Migrations;
+using ApiRessource2.Helpers;
+using ApiRessource2.Models;
+using ApiRessource2.Models.Admin;
+using ApiRessource2.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using ApiRessource2.Models;
-using ApiRessource2.Services;
-//using ApiRessource2.Migrations;
-using ApiRessource2.Helpers;
-using System.Reflection.Metadata.Ecma335;
 
 namespace ApiRessource2.Controllers
 {
@@ -25,7 +20,6 @@ namespace ApiRessource2.Controllers
             _context = context;
             _userService = userService;
         }
-
 
         [HttpPost("authenticate")]
         public IActionResult Authenticate(AuthenticateRequest model)
@@ -45,8 +39,6 @@ namespace ApiRessource2.Controllers
 
             return Ok(token);
         }
-
-
 
         // GET: api/Users
         [Authorize]
@@ -119,13 +111,13 @@ namespace ApiRessource2.Controllers
             User checkUser = _context.Users.Where(u => u.Email == postuser.Email).FirstOrDefault();
             if (checkUser != null)
             {
-                return BadRequest("L'adresse mail est deja uttilisée");
+                return BadRequest(new { message = "L'adresse mail est deja utilisée" });
             }
 
             checkUser = _context.Users.Where(u => u.Username == postuser.Username).FirstOrDefault();
             if (checkUser != null)
             {
-                return BadRequest("Le nom d'uttilisateur est deja uttilisé");
+                return BadRequest(new { message = "Le nom d'utilisateur est deja utilisé" });
             }
 
             User user = new User()
@@ -149,86 +141,20 @@ namespace ApiRessource2.Controllers
         }
 
         // DELETE: api/Users/5
-        [Authorize]
+        [Authorize(Role.Administrator, Role.SuperAdministrator)]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUser(int id)
         {
             var user = await _context.Users.FindAsync(id);
-            User usermod = (User)HttpContext.Items["User"];
-            if (usermod.Role == Role.Administrator || usermod.Role == Role.SuperAdministrator)
+            if (user == null)
             {
-                if (usermod == null)
-                {
-                    return NotFound();
-                }
-                if (user == null)
-                {
-                    return NotFound();
-                }
+                return NotFound();
             }
             user.IsDeleted = true;
             _context.Users.Update(user);
             await _context.SaveChangesAsync();
 
             return NoContent();
-        }
-
-        [Authorize]
-        [HttpPut("moderateur/{id}")]
-        public async Task<IActionResult> MakeUserModerator(int id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            User usertomakemoderator = _context.Users.Where(u=>u.Id == id).FirstOrDefault();
-            usertomakemoderator.Role = Role.Moderator;
-            _context.Update(usertomakemoderator);
-            await _context.SaveChangesAsync();
-
-            return Ok(usertomakemoderator);
-        }
-
-        [Authorize]
-        [HttpPut("admin/{id}")]
-        public async Task<IActionResult> MakeUserAdmin(int id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            User usertomakeadmin = _context.Users.Where(u => u.Id == id).FirstOrDefault();
-            usertomakeadmin.Role = Role.Administrator;
-            _context.Update(usertomakeadmin);
-            await _context.SaveChangesAsync();
-
-            return Ok(usertomakeadmin);
-        }
-
-        [Authorize]
-        [HttpPut("superadmin/{id}")]
-        public async Task<IActionResult> MakeUserSuperAdmin(int id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            User usertomakeSuperAdmin = _context.Users.Where(u => u.Id == id).FirstOrDefault();
-            usertomakeSuperAdmin.Role = Role.SuperAdministrator;
-            _context.Update(usertomakeSuperAdmin);
-            await _context.SaveChangesAsync();
-
-            return Ok(usertomakeSuperAdmin);
-        }
-
-        [Authorize]
-        [HttpGet("deleted")]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsersSuspended()
-        {
-            return await _context.Users.Where(u=>u.IsDeleted == true).ToListAsync();
         }
 
         [Authorize]
@@ -255,106 +181,41 @@ namespace ApiRessource2.Controllers
             return NoContent();
         }
 
-        [Authorize]
-        [HttpPut("makemoderator/{id}")]
-        public async Task<IActionResult> MakeModerator(int id)
+        [Authorize(Role.SuperAdministrator)]
+        [HttpPost("admin/create")]
+        public async Task<IActionResult> AdminCreate(CreateUser createUser)
         {
-            User user = _context.Users.Find(id);
-            User usersuper = (User)HttpContext.Items["User"];
-            if (usersuper.Role == Role.SuperAdministrator)
-            {
-                if (user == null)
-                {
-                    return NotFound();
-                }
-            }
-            user.Role = Role.Moderator;
-            _context.Update(user);
-            await _context.SaveChangesAsync();
-            return NoContent();
-        }
-
-        [Authorize]
-        [HttpPut("makeadmin/{id}")]
-        public async Task<IActionResult> MakeAdmin(int id)
-        {
-            User user = _context.Users.Find(id);
-            User usersuper = (User)HttpContext.Items["User"];
-            if (usersuper.Role == Role.SuperAdministrator)
-            {
-                if (user == null)
-                {
-                    return NotFound();
-                }
-            }
-            user.Role = Role.Administrator;
-            _context.Update(user);
-            await _context.SaveChangesAsync();
-            return NoContent();
-        }
-
-        [Authorize]
-        [HttpPut("makesuperadmin/{id}")]
-        public async Task<IActionResult> MakeSuperAdmin(int id)
-        {
-            User user = _context.Users.Find(id);
-            User usersuper = (User)HttpContext.Items["User"];
-            if (usersuper.Role == Role.SuperAdministrator)
-            {
-                if (user == null)
-                {
-                    return NotFound();
-                }
-            }
-            user.Role = Role.SuperAdministrator;
-            _context.Update(user);
-            await _context.SaveChangesAsync();
-            return NoContent();
-        }
-
-        [Authorize]
-        [HttpPost("createuserwithrole")]
-        public async Task<IActionResult> createUserWithRole(PostUser postuser, Role role)
-        {
-            User usersuper = (User)HttpContext.Items["User"];
-            if (usersuper.Role == Role.SuperAdministrator)
-            {
-                if (postuser == null ||  role == null)
-                {
-                    return BadRequest("Il faut envoyer les information d'un uttilisateur ET un role mon pote");
-                }
-            }
-            if (!Tools.IsEmailValid(postuser.Email))
+            if (!Tools.IsEmailValid(createUser.Email))
                 return BadRequest("Une adresse mail valide doit etre rentré.");
-            if (!Tools.IsValidPhoneNumber(postuser.PhoneNumber))
+            if (!Tools.IsValidPhoneNumber(createUser.PhoneNumber))
                 return BadRequest("Un numéro de téléphone valide doit etre rentré et doit respecter ce format : +33XXXXXXX .");
-            if (!Tools.IsValidPassword(postuser.Password))
+            if (!Tools.IsValidPassword(createUser.Password))
                 return BadRequest("Le mot de passe ne convient pas car il ne contient pas : 1 majuscule, 1 minuscule, 1 chiffre, 1 caractère spécial et 8 caractère minimum.");
             //TODO: vérifier si email et pseudo unique, a faire dans la base en modifiant les class peut etre ?
-            User checkUser = _context.Users.Where(u => u.Email == postuser.Email).FirstOrDefault();
+            User checkUser = _context.Users.Where(u => u.Email == createUser.Email).FirstOrDefault();
             if (checkUser != null)
             {
-                return BadRequest("L'adresse mail est deja uttilisée");
+                return BadRequest(new { message = "L'adresse mail est deja utilisée" });
             }
 
-            checkUser = _context.Users.Where(u => u.Username == postuser.Username).FirstOrDefault();
+            checkUser = _context.Users.Where(u => u.Username == createUser.Username).FirstOrDefault();
             if (checkUser != null)
             {
-                return BadRequest("Le nom d'uttilisateur est deja uttilisé");
+                return BadRequest(new { message = "Le nom d'utilisateur est deja utilisé" });
             }
 
             User user = new User()
             {
-                FirstName = postuser.FirstName,
-                LastName = postuser.LastName,
-                Email = postuser.Email,
-                Username = postuser.Username,
-                PhoneNumber = postuser.PhoneNumber,
-                Password = Tools.HashCode(postuser.Password),
+                FirstName = createUser.FirstName,
+                LastName = createUser.LastName,
+                Email = createUser.Email,
+                Username = createUser.Username,
+                PhoneNumber = createUser.PhoneNumber,
+                Password = Tools.HashCode(createUser.Password),
                 CreationDate = DateTime.UtcNow,
                 IsConfirmed = false,
                 IsDeleted = false,
-                Role = role,
+                Role = createUser.Role,
                 ZoneGeoId = 10
             };
             _context.Users.Add(user);
---
+++ ApiRessource2/Models/Admin/CreateUser.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiRessource2.Models.Admin
+{
+    public class CreateUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public Role Role { get; set; }
+    }
+}
